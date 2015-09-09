8f0448b0677aeee1a629ccafff950ccaaacf8efe
Fernify <dtfern@gmail.com>
2015-09-09 21:47:57 +0100

    Added Splash image on search, Separated test data
    Tried recursive ajax calling, but couldn't get $.ajax to recognise 429
    status and thus read the header

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,143 +1,135 @@
-		var champIDsDiedTo = [];
-			var summonerObj;
-			var listOfMatchIds = [];
-			var listOfMatches = [];
-			var killers = [];
-			var globalIndex = 0;
-			function PrintAllData(){
+var champIDsDiedTo = [];
+var summonerObj;
+var listOfMatchIds = [];
+var listOfMatches = [];
+var killers = [];
+var globalIndex = 0;
 
-				killers = killers.sort(function(a,b){
-					var aTot = GetKillCount(a.vicIDs);
-					var bTot = GetKillCount(b.vicIDs);
-					return (bTot > aTot) ? 1 : ((bTot < aTot) ? -1 : 0);
-				});
-				GetChampionName(killers[0].id, function(name){
-					$("#worstEnemy").text(name);
-				});
-				GetChampionNameCall(0);
-				//$.each(killers, function(index, killer){
-				//	console.log(index + " " + killer.id);
-				//	GetChampionName(killer.id, function(kName){
-				//		for(var l = 0; l < killer.vicIDs.length; l++){		
-				//			var killRecord = killer.vicIDs[l];			
-				//			var killText = "<li>" + kName + "<ul>";
-				//			GetChampionName(killRecord.vID, function(vName){
-				//				var vString = "<li>" + " killed " + vName + " " + killRecord.count + " times</li>";
-				//				killText +=  vString;
-				//				if(l == killer.vicIDs.length){
-				//					$("#matchIdDisplay").append(killText+"</ul></li>");
-				//				}
-				//			});
-				//		}
-				//	});
-				//	
-				//});
-			}
+function PrintAllData() {
+    GetChampionName(killers[0].id, function(name){
+        $("#worstEnemy").text(name);
+        var imageName  = name.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+            return letter.toUpperCase();
+        });
+        imageName = imageName.replace(" ", "");
+        $(".constrain").css("background-image", "url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + imageName + "_0.jpg')");
+    });
+    DisplayKillerChampions(0);
+}
 
-			function GetChampionNameCall(index){
-				if(index < killers.length){
-					var killer = killers[index];
-					GetChampionName(killer.id, function(kName){
-						for(var l = 0; l < killer.vicIDs.length; l++){		
-							var killRecord = killer.vicIDs[l];			
-							var killText = "<li>" + kName + "<ul>";
-							GetChampionName(killRecord.vID, function(vName){
-								var vString = "<li>" + " killed " + vName + " " + killRecord.count + " times</li>";
-								killText +=  vString;
-								if(l == killer.vicIDs.length){
-									$("#matchIdDisplay").append(killText+"</ul></li>");
-									var newIndex = index + 1;
-									GetChampionNameCall(newIndex);
-								}
-							});
-						}
-					});					
-				}
-			}
+function DisplayKillerChampions(index){
+    if(index < killers.length){
+        var killer = killers[index];
+        GetChampionName(killer.id, function(kName){
+            for(var l = 0; l < killer.vicIDs.length; l++){		
+                var killRecord = killer.vicIDs[l];			
+                var killText = "<li>" + kName + "<ul>";
+                GetChampionName(killRecord.vID, function(vName){
+                    var vString = "<li>" + " killed " + vName + " " + killRecord.count + " times</li>";
+                    killText +=  vString;
+                    if(l == killer.vicIDs.length){
+                        $("#matchIdDisplay").append(killText+"</ul></li>");
+                        var newIndex = index + 1;
+                        DisplayKillerChampions(newIndex);
+                    }
+                });
+            }
+        });					
+    }
+}
 
-			function GetKillCount(vicObjs){
-				var total = 0;
-				$.each(vicObjs, function(index, vicObj){
-					total += vicObj.count;
-				});
-				return total;
-			}
+function GetKillCount(vicObjs){
+    var total = 0;
+    $.each(vicObjs, function(index, vicObj){
+        total += vicObj.count;
+    });
+    return total;
+}
 
-			function GetAllData(){
-						GetAllMatchData(function(matches){
-							$.each(matches, function(index, match){
-								FormatMatchData(match);
-								listOfMatches.push(match);
-								if(matches.length-1 == index){
-									console.log("End");
-									PrintAllData();
-								}
-							});
-						});
+function GetAllData(){
+    GetSummonerId($("#nameText").val(), function(summoner){
+    	summonerObj = summoner;
+    	GetSummonerMatches(summoner.id, function(matches){
+    		listOfMatchIds = matches;
+            GetAllMatchData(function(listMatches){
+                $.each(listMatches, function(index, match){
+                    FormatMatchData(match);
+                    listOfMatches.push(match);
+                    if(listMatches.length-1 == index){
+                        console.log("End");
+                        PrintAllData();
+                    }
+                });
+            });
+                            
+            //GetMatchDataFromMatchIds(0, function(){
+            //    $.each(listOfMatches, function(index, match){
+            //        FormatMatchData(match);
+            //        listOfMatches.push(match);
+            //        if(matches.length-1 == index){
+            //            console.log("End");
+            //            PrintAllData();
+            //        }
+            //    });
+            //});
+    	});
+    });
+}
 
+function GetMatchDataFromMatchIds(index, callback){
+    if(index < listOfMatchIds.length){
+        var matchID = listOfMatchIds[index];
+        GetMatchData(matchID, function(matchData){
+            listOfMatches.push(matchData);
+            var newIndex = index + 1;
+            GetMatchDataFromMatchIds(newIndex);
+        });			
+    } else{
+        callback();
+    }
+}
 
-				//GetSummonerId($("#nameText").val(), function(summoner){
-				//	summonerObj = summoner;
-				//	GetSummonerMatches(summoner.id, function(matches){
-				//		listOfMatchIds = matches;
-				//		var maxIndex = matches.length;
-				//		var index = 0;
-
-						//var myInterval = setInterval(function(){
-						//	if(index == maxIndex){
-						//		console.log("Data gotten");
-						//		clearInterval(myInterval);
-						//	}else{
-						//		console.log("Match " + (index +1));
-						//		GetMatchData(matches[index], function(match){
-						//		FormatMatchData(match);
-						//		listOfMatches.push(match);
-						//		index++;
-						//	});
-						//	}
-						//}, 2200);
-				//	});
-				//});
-			}
-
-			function FormatMatchData(match){
-				var ppIDs = match.participantIdentities;
-				var ppChamps = match.participants;
-				var playerPPID = ($.grep(ppIDs, function(x){return (x.player.summonerId == 25575262);}))[0].participantId;  
-				var playerChampID = ppChamps[playerPPID-1].championId;
-				var frames = match.timeline.frames;
-				//var killerFrames = $.grep(frames, function)
-				for(var f = 0; f<frames.length; f++){
-					var currentFrame = frames[f];
-					if(currentFrame.events != undefined){
-						for(var e = 0; e<currentFrame.events.length; e++){
-							var currentEvent = currentFrame.events[e];
-							if(currentEvent.eventType == "CHAMPION_KILL"){
-								if(currentEvent.victimId == playerPPID){
-									champIDsDiedTo.push({ kID: ppChamps[currentEvent.killerId-1].championId, vID: playerChampID});
-								}	
-							}
-						}
-					}
-				}
-				$.each(champIDsDiedTo, function(index, champDeath){
-					var exists = false;
-					for(var i = 0; i < killers.length; i++){
-						var thisKiller = killers[i];
-						if(thisKiller.id == champDeath.kID){
-							exists = true;
-							for(var v = 0; v < thisKiller.vicIDs.length; v++){
-								if(thisKiller.vicIDs[v].vID == champDeath.vID){
-									thisKiller.vicIDs[v].count = thisKiller.vicIDs[v].count + 1;
-								}
-							}
-						}
-					}
-					if(!exists){
-						var newItem = {};
-						newItem.id = champDeath.kID;
-						newItem.vicIDs = [{vID:champDeath.vID, count:1}];
-						killers.push(newItem);
-					}
-				});
-			}
+function FormatMatchData(match){
+    var ppIDs = match.participantIdentities;
+    var ppChamps = match.participants;
+    var playerPPID = ($.grep(ppIDs, function(x){return (x.player.summonerId == 25575262);}))[0].participantId;  
+    var playerChampID = ppChamps[playerPPID-1].championId;
+    var frames = match.timeline.frames;
+    for(var f = 0; f<frames.length; f++){
+        var currentFrame = frames[f];
+        if(currentFrame.events != undefined){
+            var events = currentFrame.events;
+            var victimKillEvents = $.grep(events, function(e){ return (e.eventType == "CHAMPION_KILL" && e.victimId == playerPPID);});
+            if(victimKillEvents.length > 0){
+                $.each(victimKillEvents, function(index, killEvent){
+                    champIDsDiedTo.push({ kID: ppChamps[killEvent.killerId-1].championId, vID: playerChampID});
+                });
+            }
+        }
+    }
+    $.each(champIDsDiedTo, function(index, champDeath){
+        var exists = false;
+        for(var i = 0; i < killers.length; i++){
+            var thisKiller = killers[i];
+            if(thisKiller.id == champDeath.kID){
+                exists = true;
+                for(var v = 0; v < thisKiller.vicIDs.length; v++){
+                    if(thisKiller.vicIDs[v].vID == champDeath.vID){
+                        thisKiller.vicIDs[v].count = thisKiller.vicIDs[v].count + 1;
+                    }
+                }
+            }
+        }
+        if(!exists){
+            var newItem = {};
+            newItem.id = champDeath.kID;
+            newItem.vicIDs = [{vID:champDeath.vID, count:1}];
+            killers.push(newItem);
+        }
+    });
+    killers = killers.sort(function(a,b){
+        var aTot = GetKillCount(a.vicIDs);
+        var bTot = GetKillCount(b.vicIDs);
+        return (bTot > aTot) ? 1 : ((bTot < aTot) ? -1 : 0);
+    });
+}
